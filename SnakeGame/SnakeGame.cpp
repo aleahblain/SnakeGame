34aab10eac14ee65765aae8d86f7590a271f9634
aleahblain <59669691+aleahblain@users.noreply.github.com>
2026-03-04 22:07:20 +0900

    Game completed

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -2,19 +2,225 @@
 //
 
 #include <iostream>
+#include <raylib.h>
+#include <deque>
+#include <raymath.h>
+
+using namespace std;
+
+Color purple = { 216, 211, 233, 255 };
+Color darkPurple = DARKPURPLE; // { 199, 193, 219, 255 }; 
+
+int cellSize = 30;
+int cellCount = 25;
+int offset = 75;
+
+double lastUpdateTime = 0;
+
+bool eventTriggered(double interval) {
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval) {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+bool elementInDeque(Vector2 element, deque<Vector2> deque) {
+    for (unsigned int i = 0; i < deque.size(); i++) {
+        if (Vector2Equals(deque[i], element))
+            return true;
+    }
+    return false;
+}
+
+class Food {
+
+public:
+
+    Vector2 position;
+    Texture2D texture;
+
+    Food(deque<Vector2> chimataBody) {
+        Image sushi = LoadImage("sushi.png");
+        texture = LoadTextureFromImage(sushi);
+        UnloadImage(sushi);
+        position = generateRandomPos(chimataBody);
+    }
+
+    ~Food() {
+        UnloadTexture(texture);
+    }
+
+    void Draw() {
+        DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
+    }
+
+    Vector2 generateRandomcell() {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y };
+    }
+
+    Vector2 generateRandomPos(deque<Vector2> chimataBody) {
+        Vector2 position = generateRandomcell();
+        while (elementInDeque(position, chimataBody)) {
+            position = generateRandomcell();
+        }
+        return position;
+    }
+};
+
+class Player {
+public:
+
+    Texture2D headTexture;
+    Texture2D bodyTexture;
+    deque<Vector2> body = { Vector2{ 6, 9 }, Vector2{ 5, 9 }, Vector2{ 4, 9 } };
+    Vector2 direction = { 1, 0 };
+    bool addPlate = false;
+
+    Player() {
+        Image chimata = LoadImage("chimata.png");
+        headTexture = LoadTextureFromImage(chimata);
+        UnloadImage(chimata);
+        Image plate = LoadImage("plate.png");
+        bodyTexture = LoadTextureFromImage(plate);
+        UnloadImage(plate);
+    }
+
+    ~Player() {
+        UnloadTexture(headTexture);
+        UnloadTexture(bodyTexture);
+    }
+
+    void Draw() {
+        
+        for (unsigned int i = 0; i < body.size(); i++) {
+            int x = body[i].x;
+            int y = body[i].y;
+            if (i > 0) {
+                DrawTexture(bodyTexture, offset + x * cellSize, offset + y * cellSize + 30, WHITE);
+            }
+            else {
+                DrawTexture(headTexture, offset + x * cellSize, offset + y * cellSize, WHITE);
+            }
+            
+        }
+    }
+
+    void Update() {
+        body.push_front(Vector2Add(body[0], direction));
+        if (addPlate == true) {
+            addPlate = false;
+        }
+        else {
+            body.pop_back();
+        }
+    }
+
+    void reset() {
+        body = { Vector2{ 6, 9 }, Vector2{ 5, 9 }, Vector2{ 4, 9 } };
+        direction = { 1, 0 };
+    }
+};
+
+class Game {
+public:
+    Player chimata = Player();
+    Food food = Food(chimata.body);
+    bool isRunning = true;
+    int score = 0;
+    
+    void Draw() {
+        food.Draw();
+        chimata.Draw();
+    }
+
+    void Update() {
+        if (isRunning) {
+            chimata.Update();
+            checkFoodCollision();
+            checkEdgeCollision();
+            checkPlateCollision();
+        }
+    }
+
+    void checkFoodCollision() {
+        if (Vector2Equals(chimata.body[0], food.position)) {
+            food.position = food.generateRandomPos(chimata.body);
+            chimata.addPlate = true;
+            score++;
+        }
+    }
+
+    void checkEdgeCollision() {
+        if (chimata.body[0].x == cellCount || chimata.body[0].x == -1) {
+            gameOver();
+        }
+        if (chimata.body[0].y == cellCount || chimata.body[0].y == -1) {
+            gameOver();
+        }
+    }
+
+    void checkPlateCollision() {
+        deque<Vector2> noPlates = chimata.body;
+        noPlates.pop_front();
+        if (elementInDeque(chimata.body[0], noPlates)) {
+            gameOver();
+        }
+    }
+
+    void gameOver() {
+        chimata.reset();
+        food.position = food.generateRandomPos(chimata.body);
+        isRunning = false;
+        score = 0;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    InitWindow(2 * offset + cellSize * cellCount, 2 * offset + cellSize * cellCount, "Chimata Sushi Game");
+    SetTargetFPS(60);
+
+    Game game = Game();
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        if (eventTriggered(0.2)) {
+            game.Update();
+        }
+
+        // Control player
+        if (IsKeyPressed(KEY_LEFT) && game.chimata.direction.x != 1) {
+            game.chimata.direction = { -1, 0 };
+            game.isRunning = true;
+        }
+        if (IsKeyPressed(KEY_RIGHT) && game.chimata.direction.x != -1) {
+            game.chimata.direction = { 1, 0 };
+            game.isRunning = true;
+        }
+        if (IsKeyPressed(KEY_UP) && game.chimata.direction.y != 1) {
+            game.chimata.direction = { 0, -1 };
+            game.isRunning = true;
+        }
+        if (IsKeyPressed(KEY_DOWN) && game.chimata.direction.y != -1) {
+            game.chimata.direction = { 0, 1 };
+            game.isRunning = true;
+        }
+
+        // Drawing BG
+        ClearBackground(purple);
+        DrawRectangleLinesEx(Rectangle{ (float)offset - 5, (float)offset - 5, (float)cellSize * cellCount + 10, (float)cellSize * cellCount + 10 }, 5, darkPurple);
+        DrawText("Feed Chimata!", offset - 5, 20, 40, darkPurple);
+        DrawText(TextFormat("Sushi Eaten: %i", game.score), offset - 5, offset + cellSize * cellCount + 10, 30, darkPurple);
+        game.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
